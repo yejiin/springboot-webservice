1ab687a47776a6bd9850ee8a45222c0fdd1c761c
yejiin <yj_song67@naver.com>
2021-10-24 18:59:49 +0900

    mustache로 화면 구현

---
+++ src/main/java/com/example/webservice/web/dto/PostsListResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.webservice.web.dto;
+
+import com.example.webservice.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = toStringDateTime(entity.getModifiedDate());
+    }
+
+    private String toStringDateTime(LocalDateTime localDateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return Optional.ofNullable(localDateTime)
+                .map(formatter::format)
+                .orElse("");
+    }
+}
