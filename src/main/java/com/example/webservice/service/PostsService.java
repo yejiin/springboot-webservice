3ea3b5602f771bdab67d3693bec4a6cb3208fb03
yejiin <yj_song67@naver.com>
2021-10-24 16:08:07 +0900

    등록, 수정, 조회 api

---
+++ src/main/java/com/example/webservice/service/PostsService.java
@@ -0,0 +1,36 @@
+package com.example.webservice.service;
+
+import com.example.webservice.domain.posts.Posts;
+import com.example.webservice.domain.posts.PostsRepository;
+import com.example.webservice.web.dto.PostsResponseDto;
+import com.example.webservice.web.dto.PostsSaveRequestDto;
+import com.example.webservice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
