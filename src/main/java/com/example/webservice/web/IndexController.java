871984aa609d8fccd57e01f5975424e7eebc41a4
yejiin <yj_song67@naver.com>
2021-10-26 01:10:53 +0900

    @LoginUser 어노테이션 생성하여 user 세션값 가져오기

--- src/main/java/com/example/webservice/web/IndexController.java
+++ src/main/java/com/example/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.webservice.web;
 
+import com.example.webservice.config.auth.LoginUser;
 import com.example.webservice.config.auth.dto.SessionUser;
 import com.example.webservice.service.PostsService;
 import com.example.webservice.web.dto.PostsResponseDto;
@@ -16,13 +17,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
