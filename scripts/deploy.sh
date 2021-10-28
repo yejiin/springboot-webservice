d16408be8b1fee3c44bba2f3801cd6b3231ff00f
yejiin <yj_song67@naver.com>
2021-10-28 13:13:32 +0900

    무중단 배포 스크립트 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,12 +7,12 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 햔제 구동중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl webservice | grep jar | awk '{print $1}')
 
 
-echo "> 형재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
 	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
